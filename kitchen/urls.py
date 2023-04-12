42ed4a0f381ba092f8239311f9fcefbeb879930a
Alina Boichenko <ilub1108@gmail.com>
2023-04-12 13:51:40 +0300

    Added path to UpdateViews for Dish, DishType and Cook models

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -5,12 +5,15 @@ from kitchen.views import (
     DishTypeListView,
     DishTypeDetailView,
     DishTypeCreateView,
+    DishTypeUpdateView,
     DishListView,
     DishDetailView,
     DishCreateView,
+    DishUpdateView,
     CookListView,
     CookDetailView,
-    CookCreateView
+    CookCreateView,
+    CookUpdateView
 )
 
 urlpatterns = [
@@ -26,12 +29,19 @@ urlpatterns = [
         DishTypeCreateView.as_view(),
         name="dish-type-create"
     ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
     path("dishes/", DishListView.as_view(), name="dish-list"),
-    path("dishes/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
+    path("dishes/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
     path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
+    path("cooks/<int:pk>/update/", CookUpdateView.as_view(), name="cook-update"),
 ]
 
 app_name = "kitchen"
