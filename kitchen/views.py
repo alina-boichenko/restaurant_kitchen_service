320965e81714ab5b7c935e64480c2c6f3123f8a2
Alina Boichenko <ilub1108@gmail.com>
2023-04-15 12:30:46 +0300

    Added view add_or_delete_dish and added methods for search form for ListViews of Dish, DishType, Cook models

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.db.models import QuerySet
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from kitchen.forms import DishForm, CookCreationForm
+from kitchen.forms import DishForm, CookCreationForm, DishTypeSearchForm, DishSearchForm, CookSearchForm
 from kitchen.models import Cook, Dish, DishType
 
 
@@ -26,9 +29,33 @@ class DishTypeListView(ListView):
     context_object_name = "dish_type_list"
     ordering = ["name"]
 
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        context = super(DishTypeListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = DishTypeSearchForm(initial={
+            "name": name,
+        })
+
+        return context
+
+    def get_queryset(self) -> QuerySet:
+        queryset = DishType.objects.prefetch_related("dishes")
+
+        form = DishTypeSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+
+        return queryset
+
 
 class DishTypeDetailView(DetailView):
     model = DishType
+    queryset = DishType.objects.prefetch_related("dishes")
     template_name = "kitchen/dish_type_detail.html"
     context_object_name = "dish_type"
 
@@ -59,7 +86,29 @@ class DishListView(ListView):
     ordering = ["name"]
     paginate_by = 5
 
-    queryset = Dish.objects.prefetch_related("cooks")
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        context = super(DishListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = DishSearchForm(initial={
+            "name": name
+        })
+
+        return context
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Dish.objects.prefetch_related("cooks")
+
+        form = DishSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+
+        return queryset
 
 
 class DishDetailView(DetailView):
@@ -84,9 +133,47 @@ class DishDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy("kitchen:dish-list")
 
 
+@login_required
+def add_or_delete_dish(request, pk):
+    dish = get_object_or_404(Dish, id=pk)
+    cook = Cook.objects.get(pk=request.user.id)
+
+    if cook in dish.cooks.all():
+        dish.cooks.remove(cook)
+    else:
+        dish.cooks.add(cook)
+
+    return HttpResponseRedirect(
+        reverse("kitchen:dish-detail", args={dish.pk})
+    )
+
+
 class CookListView(ListView):
     model = Cook
     paginate_by = 6
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        context = super(CookListView, self).get_context_data(**kwargs)
+
+        username = self.request.GET.get("username", "")
+
+        context["search_form"] = CookSearchForm(initial={
+            "username": username
+        })
+
+        return context
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Cook.objects.prefetch_related("dishes")
+
+        form = CookSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+
+        return queryset
 
 
 class CookDetailView(DetailView):
