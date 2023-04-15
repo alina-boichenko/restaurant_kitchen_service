b7042d1bf75477cd71ae2e34888a9b5f3de139b4
Alina Boichenko <ilub1108@gmail.com>
2023-04-15 13:13:50 +0300

    Fixed flake8

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -46,14 +46,30 @@ urlpatterns = [
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
-    path("dishes/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
-    path("dishes/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
+    path(
+        "dishes/<int:pk>/update/",
+        DishUpdateView.as_view(),
+        name="dish-update"
+    ),
+    path(
+        "dishes/<int:pk>/delete/",
+        DishDeleteView.as_view(),
+        name="dish-delete"
+    ),
     path("dishes/<int:pk>/add-dishes/", add_or_delete_dish, name="add-dish"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
     path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
-    path("cooks/<int:pk>/update/", CookUpdateView.as_view(), name="cook-update"),
-    path("cooks/<int:pk>/delete/", CookDeleteView.as_view(), name="cook-delete"),
+    path(
+        "cooks/<int:pk>/update/",
+        CookUpdateView.as_view(),
+        name="cook-update"
+    ),
+    path(
+        "cooks/<int:pk>/delete/",
+        CookDeleteView.as_view(),
+        name="cook-delete"
+    ),
 ]
 
 app_name = "kitchen"
