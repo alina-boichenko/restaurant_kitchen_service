746c17bece374a831b3116dff91cbaef17f30bb3
Alina Boichenko <ilub1108@gmail.com>
2023-05-02 23:10:00 +0300

    Deleted MEDIA_ROOT

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -138,9 +138,6 @@ STATICFILES_DIRS = [BASE_DIR / "static",]
 
 STATIC_ROOT = "staticfiles/"
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
